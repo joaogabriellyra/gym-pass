4d7ecd8e4395ee1d532bf3dc3fbcc173cfc40ed6
joaogabriellyra <jgabriellyra@hotmail.com>
2024-12-12 21:19:44 -0300

    add a vitest method to mock data to check in

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import { beforeEach, afterEach, describe, expect, it, vi } from 'vitest'
 import { CheckInUseCase } from './check-in'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins.repository'
 
@@ -9,13 +9,36 @@ describe('Check-in User Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
   })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
   it('should be able to check in', async () => {
-    const checkIn = await checkInsRepository.create({
-      gym_id: 'gym-id-1',
-      user_id: 'user-id-1',
+    vi.setSystemTime(new Date(2020, 11, 22, 20))
+
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-id-1',
+      userId: 'user-id-1',
+    })
+    console.log(checkIn.created_at)
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice in the same day', async () => {
+    await sut.execute({
+      gymId: 'gym-id-1',
+      userId: 'user-id-1',
     })
 
-    expect(checkIn.id).toEqual(expect.any(String))
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-id-1',
+        userId: 'user-id-1',
+      })
+    ).rejects.toBeInstanceOf(Error)
   })
 })
