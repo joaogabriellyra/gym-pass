f6bf016da321e6e75e517e24ad94adbf816a7a78
joaogabriellyra <jgabriellyra@hotmail.com>
2025-01-03 04:01:36 -0300

    implements the create method in gyms in memory repository

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,8 +1,24 @@
-import type { Gym } from '@prisma/client'
+import type { Gym, Prisma } from '@prisma/client'
 import type { IGymsRepository } from '../gyms-repository'
+import { randomUUID } from 'node:crypto'
+import { Decimal } from '@prisma/client/runtime/library'
 
 export class InMemoryGymsRepository implements IGymsRepository {
   public gyms: Gym[] = []
+
+  async create(data: Prisma.GymCreateInput): Promise<Gym> {
+    const gym = {
+      description: data.description ?? null,
+      latitude: new Decimal(data.latitude.toString()),
+      longitude: new Decimal(data.longitude.toString()),
+      title: data.title,
+      phone: data.phone ?? null,
+      id: randomUUID(),
+    }
+
+    this.gyms.push(gym)
+    return gym
+  }
   async findOneById(gymId: string): Promise<Gym | null> {
     const gym = this.gyms.find(gym => gym.id === gymId)
     if (!gym) {
