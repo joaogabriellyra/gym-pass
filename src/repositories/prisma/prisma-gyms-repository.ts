04ff732eace3935ca2a6d2d8562b6b1775448e33
joaogabriellyra <jgabriellyra@hotmail.com>
2025-01-15 13:18:45 -0300

    implements searchMany method in the prisma gyms repository

--- src/repositories/prisma/prisma-gyms-repository.ts
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -15,8 +15,22 @@ export class PrismaGymsRepository implements IGymsRepository {
 
     return gym
   }
-  searchMany(query: string, page: number): Promise<Gym[]> {
-    throw new Error('Method not implemented.')
+  async searchMany(query: string, page: number): Promise<Gym[]> {
+    const manyGyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+          mode: 'insensitive',
+        },
+      },
+      orderBy: {
+        title: 'asc',
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return manyGyms
   }
   async create(data: Prisma.GymCreateInput): Promise<Gym> {
     const gym = await prisma.gym.create({
