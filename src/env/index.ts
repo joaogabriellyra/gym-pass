c150d728466fd32b85c8f528c9b3717b6c3392bc
joaogabriellyra <jgabriellyra@hotmail.com>
2025-01-16 16:52:17 -0300

    insert the jwt verification

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,8 +1,11 @@
-import z from "zod";
+import z from 'zod'
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
-  PORT: z.coerce.number().default(3333)
+  NODE_ENV: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
+  PORT: z.coerce.number().default(3333),
+  JWT_SECRET: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
