25b75469f3f71cb43246a565be8459ce635ed407
joaogabriellyra <jgabriellyra@hotmail.com>
2025-01-24 20:02:02 -0300

    add a middleware responsible for verify the JWT

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,27 +1,15 @@
-import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
 import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile'
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import z from 'zod'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  // const profileBodySchema = z.object({
-  //   userId: z.string().uuid(),
-  // })
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  // const { userId } = profileBodySchema.parse(request.body)
+  const { user } = await getUserProfile.execute({ userId: request.user.sub })
 
-  // try {
-  //   const profileteUseCase = makeGetUserProfileUseCase()
-
-  //   const { user } = await profileteUseCase.execute({ userId })
-
-  //   return reply.status(200).send(user)
-  // } catch (error) {
-  //   if (error instanceof InvalidCredentialsError) {
-  //     return reply.status(400).send({ message: error.message })
-  //   }
-  //   throw error
-  // }
-
-  return reply.status(200).send()
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
