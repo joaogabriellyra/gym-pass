912ce4ebc0bb7dfc3cd311d9cc86c8366e5a154b
joaogabriellyra <jgabriellyra@hotmail.com>
2025-01-29 01:44:14 -0300

    creates the create gym controller

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,8 +1,8 @@
 import type { FastifyInstance } from 'fastify'
-import { createGym } from './create-gym'
+import { create } from './create'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
-  app.post('/create-gym', createGym)
+  app.post('/create-gym', create)
 }
