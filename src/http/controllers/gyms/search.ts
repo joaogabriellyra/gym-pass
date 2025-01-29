8618bd5d73b76ef87bac9e18cbdbceb804202072
joaogabriellyra <jgabriellyra@hotmail.com>
2025-01-29 01:53:54 -0300

    creates the nearby gyms controller

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -1,5 +1,3 @@
-import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
-import { makeCreateGymUseCase } from '@/use-cases/factories/make-create-gym-use-case'
 import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
