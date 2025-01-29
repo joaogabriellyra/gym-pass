3e0fb855b27e9a831ea1f57f6b67ae36d6c1b624
joaogabriellyra <jgabriellyra@hotmail.com>
2025-01-29 16:54:53 -0300

    creates the check-ins routes

---
+++ src/http/controllers/check-ins/routes.ts
@@ -0,0 +1,7 @@
+import type { FastifyInstance } from 'fastify'
+
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+
+export async function checkInsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJWT)
+}
--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -9,7 +9,7 @@ import { nearby } from './nearby'
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
-  app.post('/gyms/crate', create)
+  app.post('/gyms/create', create)
 
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
