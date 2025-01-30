a291d1476c0da907f8050ec3014d0c3b03e881c9
joaogabriellyra <jgabriellyra@hotmail.com>
2025-01-30 02:46:14 -0300

    creates the validate check-in route

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -2,9 +2,11 @@ import type { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { create } from './create'
+import { validate } from './validate'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.post('/check-ins/create', create)
+  app.post('/check-ins/validate', validate)
 }
