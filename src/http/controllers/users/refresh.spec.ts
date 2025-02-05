a5f15eac82d2d31d29b0d6ed68713ec1948c3d75
joaogabriellyra <jgabriellyra@hotmail.com>
2025-02-05 19:53:39 -0300

    tests the refresh token route

---
+++ src/http/controllers/users/refresh.spec.ts
@@ -0,0 +1,44 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+
+describe('Refresh (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to refresh a token', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password: '123456',
+    })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'john.doe@gmail.com',
+      password: '123456',
+    })
+
+    const cookies = authResponse.get('Set-Cookie')
+
+    if (!cookies) {
+      throw new Error('No cookies returned from authentication response')
+    }
+
+    const response = await request(app.server)
+      .patch('/token/refresh')
+      .set('Cookie', cookies)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+    expect(response.get('Set-Cookie')).toEqual([
+      expect.stringContaining('refreshToken='),
+    ])
+  })
+})
